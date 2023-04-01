8900766a84c791a88c12be05227f69cd09d847aa
Pia <76558220+rkdud007@users.noreply.github.com>
2023-04-01 15:34:44 +0100

    add semaphore on deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import "../src/Contract.sol";
+import "../src/Semaphore.sol";
 import "../src/interfaces/IWorldID.sol";
 
 contract MyScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Contract c = new Contract(IWorldID(0x0deCadAeEb36661cbB9ec3e3344a134cbe61C4CC), "my_action");
+        Semaphore c_semaphore = new Semaphore();
+
+        Contract c = new Contract(IWorldID(address(c_semaphore)), "my_action");
         vm.stopBroadcast();
     }
 }
