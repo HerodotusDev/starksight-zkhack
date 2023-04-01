f67320f26ee2fa3aef4e2eebbf0344c1bb7ebc80
tiagofneto <tiagofneto29@gmail.com>
2023-04-01 22:12:19 +0100

    refactoring

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "../src/Contract.sol";
+import "../src/WorldIDVerifier.sol";
 import "../src/Semaphore.sol";
 import "../src/interfaces/IWorldID.sol";
 
@@ -10,9 +10,10 @@ contract MyScript is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Semaphore c_semaphore = new Semaphore();
 
-        Contract c = new Contract(IWorldID(address(c_semaphore)), "app_id", 123);
+        Semaphore c_semaphore = new Semaphore();
+        WorldIDVerifier c = new WorldIDVerifier(IWorldID(address(c_semaphore)), "app_id", 123);
+
         vm.stopBroadcast();
     }
 }
